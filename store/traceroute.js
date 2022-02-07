30ecc743422969cad738ee57f9b87cce702c2495
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2022-02-07 20:40:16 +0900

    Add hops column

--- store/traceroute.js
+++ store/traceroute.js
@@ -25,23 +25,42 @@ export const actions = {
   },
 }
 
+const trace2hops_str = (trace) => {
+  return trace.hops
+    .map((hop) => {
+      const step = hop.steps.find((step) => step.action == "RECEIVED")
+      return `${hop.node}[${step.detail.inputInterface}]`
+    })
+    .join(" -> ")
+}
+
+const item2table = (item, result_string) => {
+  return {
+    network: item.network,
+    snapshot: item.snapshot,
+    description: item.description,
+    result: result_string,
+    hops: item?.result?.Traces?.length > 0 ? trace2hops_str(item.result.Traces[0]) : "None",
+  }
+}
+
+const is_trace_result = (item, disposition) => {
+  return !!item.result.Traces.find((trace) => trace.disposition == disposition)
+}
+
 export const getters = {
   getResults: (state) => {
     return state.results.map((item) => {
       if (!Object.keys(item.result).length) {
-        return { network: item.network, snapshot: item.snapshot, description: "processing...", result: "processing..." }
+        return item2table(item, "processing")
       }
-      if (item.result["Traces"].filter((x) => x.disposition == "ACCEPTED").length > 0) {
-        return { network: item.network, snapshot: item.snapshot, description: item.description, result: "reachable" }
-      } else if (item.result["Traces"].filter((x) => x.disposition == "DISABLED").length > 0) {
-        return { network: item.network, snapshot: item.snapshot, description: item.description, result: "DISABLED" }
+
+      if (is_trace_result(item, "ACCEPTED")) {
+        return item2table(item, "Reachable")
+      } else if (is_trace_result(item, "DISABLED")) {
+        return item2table(item, "DISABLED")
       } else {
-        return {
-          network: item.network,
-          snapshot: item.snapshot,
-          description: item.description,
-          result: "U N R E A C H A B L E",
-        }
+        return item2table(item, "U N R E A C H A B L E")
       }
     })
   },
